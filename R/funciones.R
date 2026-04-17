1a3d685c3b354486a2af3f92af63ab187a819ce2
danielanavarrete2026 <daniela.navarrete.20241@americanbusiness.college>
2026-04-16 22:36:42 -0400

    Create funciones.R

---
+++ R/funciones.R
@@ -0,0 +1,11 @@
+suma.dos.numeros <- function(a, b) {
+  return(a + b)
+}
+
+resta.dos.numeros <- function(a, b) {
+  return(a - b)
+}
+
+multiplica.dos.numeros <- function(a, b) {
+  return(a * b)
+}
